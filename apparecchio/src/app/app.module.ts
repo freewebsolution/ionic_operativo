bd407e878703d1c8fcc081b5758f5f150fb4821d
Your Name <you@example.com>
2022-02-15 12:05:22 +0100

    Adattare il layout alla dimensione dello schermo

--- apparecchio/src/app/app.module.ts
+++ apparecchio/src/app/app.module.ts
@@ -7,18 +7,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { apiUrl } from './config/apiUrl';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
   ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,FontAwesomeModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    {provide:'apiUrl', useValue:apiUrl}
+    { provide: 'apiUrl', useValue: apiUrl }
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
+}
