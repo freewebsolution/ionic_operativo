aa2b1765a08233e054dd9f1b3b9fad890e479987
Lucio <info@freewebsolution.it>
2022-02-28 11:52:50 +0100

    Messaggi di avanzamento

--- apparecchio/src/app/home/home.page.ts
+++ apparecchio/src/app/home/home.page.ts
@@ -1,7 +1,9 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Piatto } from '../models/piatto';
 import { PiattiService } from '../core/services/piatti.service';
 import { InsalatoneService } from '../core/services/insalatone.service';
+import { LoadingController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -9,15 +11,17 @@ import { InsalatoneService } from '../core/services/insalatone.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  private piatti: Piatto[];
-  private insalate: Piatto[];
-  private piattierrMsg: string;
+  piatti: Piatto[];
+  insalate: Piatto[];
+  piattierrMsg: string;
   constructor(
     private piattiService: PiattiService,
-    private insalateService: InsalatoneService
-     ) { }
+    private insalateService: InsalatoneService,
+    private loading: LoadingController
+  ) { }
 
   ngOnInit(): void {
+    this.showLoading();
     this.piattiService.getPizzaInEvidenza().subscribe(
       piatti => this.piatti = piatti,
       errMsg => this.piattierrMsg = errMsg
@@ -26,6 +30,13 @@ export class HomePage implements OnInit {
       insalate => this.insalate = insalate,
       errMsg => this.piattierrMsg = errMsg
     );
+  }
+  async showLoading() {
+    const load = await this.loading.create({
+      message: 'Caricamento in corso',
+      duration: 2000
+    });
+    await load.present();
   }
 
 }
